9de8e593b6d733f9522a052fbf755a47b01fa9f2
pilao <zeffy@users.noreply.github.com>
2020-05-31 04:11:36 -0700

    add support for sv

--- src/pluginloader/dllmain.cpp
+++ src/pluginloader/dllmain.cpp
@@ -10,65 +10,64 @@
 namespace fs = std::filesystem;
 #include <unordered_map>
 
-typedef void(__cdecl* PFN_PLUGIN_INIT)(void);
-struct PLUGIN_INFO {
-  const wchar_t* pwzName;
-  const wchar_t* pwzVersion;
-  const wchar_t* pwzDescription;
-  PFN_PLUGIN_INIT pfnInit;
-};
+typedef struct _PLUGIN_INFO {
+	const wchar_t* pwzName;
+	const wchar_t* pwzVersion;
+	const wchar_t* pwzDescription;
+	void(__cdecl* pfnInit)(void);
+} PLUGIN_INFO;
 typedef void(__cdecl* PFN_GETPLUGININFO)(PLUGIN_INFO*);
 
 std::unordered_map<std::wstring, PLUGIN_INFO> _plugins;
 
 VOID NTAPI ApcLoadPlugins(ULONG_PTR Parameter) {
-  WIN32_FIND_DATAW findFileData;
-  const auto folder = fs::path(pe::get_module()->full_name()).remove_filename().append(xorstr_(L"plugins"));
-  const auto filter = folder / xorstr_(L"*.dll");
-  auto hFindFile = FindFirstFileW(filter.c_str(), &findFileData);
-  if (hFindFile) {
-    do {
-      PLUGIN_INFO pluginInfo;
-      memset(&pluginInfo, 0, sizeof pluginInfo);
+	WIN32_FIND_DATAW findFileData;
+	const auto folder = fs::path(pe::get_module()->full_name()).remove_filename().append(xorstr_(L"plugins"));
+	const auto filter = folder / xorstr_(L"*.dll");
+	auto hFindFile = FindFirstFileW(filter.c_str(), &findFileData);
+	if (hFindFile) {
+		do {
+			PLUGIN_INFO pluginInfo;
+			memset(&pluginInfo, 0, sizeof pluginInfo);
 
-      const auto path = folder / findFileData.cFileName;
-      auto hModule = LoadLibraryExW(path.c_str(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
-      if (!hModule)
-        continue;
-      auto pfnGetPluginInfo = (PFN_GETPLUGININFO)GetProcAddress(hModule, xorstr_("GetPluginInfo"));
-      if (!pfnGetPluginInfo) {
-        FreeLibrary(hModule);
-        continue;
-      }
-      pfnGetPluginInfo(&pluginInfo);
-      if (pluginInfo.pfnInit)
-        pluginInfo.pfnInit();
-      _plugins[findFileData.cFileName] = pluginInfo;
-    } while (FindNextFileW(hFindFile, &findFileData));
-    FindClose(hFindFile);
-  }
+			const auto path = folder / findFileData.cFileName;
+			auto hModule = LoadLibraryExW(path.c_str(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+			if (!hModule)
+				continue;
+			auto pfnGetPluginInfo = (PFN_GETPLUGININFO)GetProcAddress(hModule, xorstr_("GetPluginInfo"));
+			if (!pfnGetPluginInfo) {
+				FreeLibrary(hModule);
+				continue;
+			}
+			pfnGetPluginInfo(&pluginInfo);
+			if (pluginInfo.pfnInit)
+				pluginInfo.pfnInit();
+			_plugins[findFileData.cFileName] = pluginInfo;
+		} while (FindNextFileW(hFindFile, &findFileData));
+		FindClose(hFindFile);
+	}
 }
 
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, LPVOID lpvReserved)
 {
-  if (fdwReason == DLL_PROCESS_ATTACH) {
-    DisableThreadLibraryCalls(hInstance);
-    QueueUserAPC(&ApcLoadPlugins, NtCurrentThread(), 0);
-  }
-  return TRUE;
+	if (fdwReason == DLL_PROCESS_ATTACH) {
+		DisableThreadLibraryCalls(hInstance);
+		QueueUserAPC(&ApcLoadPlugins, NtCurrentThread(), 0);
+	}
+	return TRUE;
 }
 
 ExternC const PfnDliHook __pfnDliNotifyHook2 = [](unsigned dliNotify, PDelayLoadInfo pdli) -> FARPROC
 {
-  NtTestAlert();
-  if (dliNotify == dliNotePreLoadLibrary) {
-    const auto module = pe::instance_module();
-    if (!_stricmp(pdli->szDll, module->export_directory()->name())) {
-      if (std::wstring result; SUCCEEDED(wil::GetSystemDirectoryW(result))) {
-        const auto path = fs::path(result).append(pdli->szDll);
-        return (FARPROC)LoadLibraryExW(path.c_str(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
-      }
-    }
-  }
-  return nullptr;
+	if (dliNotify == dliNotePreLoadLibrary) {
+		const auto module = pe::instance_module();
+		if (!_stricmp(pdli->szDll, module->export_directory()->name())) {
+			NtTestAlert();
+			if (std::wstring result; SUCCEEDED(wil::GetSystemDirectoryW(result))) {
+				const auto path = fs::path(result).append(pdli->szDll);
+				return (FARPROC)LoadLibraryExW(path.c_str(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+			}
+		}
+	}
+	return nullptr;
 };
